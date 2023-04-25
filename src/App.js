39c241e6bdb170f765b2bff93c9dbb49a753eb87
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-04-25 22:59:32 +0200

    Fix sign form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Splash } from './component/splash';
+import { SignUpTaxi } from './component/signUpTaxi';
+import { SignInTaxi } from './component/signInTaxi';
 
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Splash />} />
+          <Route exact path="/signUpTaxi" element={<SignUpTaxi />} />
+          <Route exact path="/signInTaxi" element={<SignInTaxi />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
