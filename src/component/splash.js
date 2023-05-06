89a5a24ce656c63de65826942c3ec6dbc35c4c56
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-05-06 15:08:59 +0200

    Upadate splash page

--- src/component/splash.js
+++ src/component/splash.js
@@ -8,19 +8,23 @@ export function Splash() {
     <div className="wrapper-splash">
       <div className="splash-container">
         <div className="container-logo"></div>
-        <div className="container-button">
-          <Link to="/signUpTaxi">
-            <button type="button" class="btn btn-success">
-              <i class="fa-sharp fa-solid fa-taxi"></i>
-            </button>
-          </Link>
-
-          <Link to="/signUpUser">
-            <button type="button" class="btn btn-success">
-              <i class="fa-solid fa-user"></i>
-            </button>
-          </Link>
-        </div>
+          <div class="row">
+            <div class="col">
+              <Link to="/signUpUser">
+                <button type="button" className="btn btn-success" style={{width:"100%"}}>
+                  <i className="fas fa-user"></i><br/> Area Utenti
+                </button>
+              </Link>
+            </div>
+            <div class="col">
+              <Link to="/signUpTaxi">
+                <button type="button" className="btn btn-success" style={{width:"100%"}}>
+                  <i className="fas fa-taxi"></i><br/> Area Conducenti
+                </button>
+              </Link>
+            </div>
+          </div>
+         
       </div>
     </div>
   );
