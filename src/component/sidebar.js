3883db0913b42f7905f16706d3393c78c48dbb8e
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-05-06 09:56:42 +0200

    Update

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import "../css/sidebar.css";
 import SideNav, {
   Toggle,
@@ -10,30 +11,32 @@ import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
   const navigate = useNavigate();
+  const [selected, setSelected] = useState('homeUser');
   return (
     <SideNav
       onSelect={(selected) => {
         console.log(selected);
+        setSelected(selected);
         navigate('/'+selected)
       }}
       className='sidenav'
     >
 
       <Toggle/>
-      <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="homeUser">
+      <SideNav.Nav defaultSelected="/">
+        <NavItem eventKey="homeUser" selected={selected === 'homeUser'}>
           <NavIcon><i className="fa fa-fw fa-home" style={{fontSize: "1.5em"}}></i></NavIcon>
           <NavText>Home</NavText>
         </NavItem>
-        <NavItem eventKey="wallet">
+        <NavItem eventKey="wallet" selected={selected === 'wallet'}>
           <NavIcon> <i className="fa fa-wallet" style={{fontSize: "1.5em"}}></i></NavIcon>
           <NavText>Wallet</NavText>
         </NavItem>        
-        <NavItem eventKey="storico">
+        <NavItem eventKey="storico" selected={selected === 'storico'}>
           <NavIcon><i className="fa fa-history" style={{fontSize: "1.5em"}}></i></NavIcon>
           <NavText>Storico</NavText>
         </NavItem>
-        <NavItem eventKey="account">
+        <NavItem eventKey="account" selected={selected === 'account'}>
           <NavIcon> <i className="fa fa-user" style={{fontSize: "1.5em"}}></i></NavIcon>
           <NavText>Profilo</NavText>
         </NavItem>  
