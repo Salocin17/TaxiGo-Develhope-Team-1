b90355d68953419ee1c58225a8179a5bce4cb38f
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-05-14 18:51:36 +0200

    Fix authentication

--- src/component/signUpUser.js
+++ src/component/signUpUser.js
@@ -1,85 +1,122 @@
 import "../css/signUp.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faLock,
-} from "@fortawesome/free-solid-svg-icons";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Link } from "react-router-dom";
-import { useState } from "react"; 
+import { useState, useEffect } from "react";
 
 export function SignUpUser() {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleSubmit = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
 
-    console.log("Username: ", username);
-    console.log("Password: ", password);
+    const data = {
+      type: true,
+      email: email,
+      password,
+    };
 
-    // codice per inviare i dati al server 
-  };
+    fetch("http://federicov.ddns.net:3300/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.status === 200) {
+        res.json().then((json) => {
+          localStorage.setItem("token", json.token);
+        });
+        window.location.href = "/homeUser";
+      }else{
+        const snackbar = document.createElement("div");
+        snackbar.className = "alert alert-danger";
+        snackbar.role = "alert";
+        snackbar.textContent = "Credenziali errate";
+        document.body.appendChild(snackbar);
+      }
+    });
+  }
 
   return (
-    <div className="wrapper">
-    <div className="container-sign-up">
-    <div className="container-logo-sign-up">
-      <div className="img-sign-up"></div>
-    </div>
-      <div className="wrapper-header">
-        <Link to="/signUpUser">
-        <a href="Sign Up" className="underline-link">Sign Up</a>
-        </Link>
-        <Link to="/signInUser">
-          <a href="Sign In">Sign In</a>
-        </Link>
+    <div className="wrapper-signUp">
+      <div className="container-sign-up">
+        <div className="img-sign-up"></div>
+        <div className="wrapper-sign-up">
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            <i class="fa fa-chevron-circle-left" aria-hidden="true"></i>
+            <b> Back</b>
+          </Link>
+          <div className="wrapper-header-sign-up">
+            <Link to="/signInUser">
+              <a
+                href="Sign Up"
+                style={{
+                  display: windowWidth <= 768 ? "none" : "inline-block",
+                }}
+              >
+                Registrati
+              </a>
+            </Link>
+            <Link to="/signUpUser">
+              <a href="Sign In" className="underline-link">
+                Accedi
+              </a>
+            </Link>
+          </div>
+
+          <form className="wrapper-form-sign-up" onSubmit={handleSubmit}>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faUser} />
+                </span>
+              </div>
+              <input
+                type="email"
+                class="form-control"
+                id="username"
+                placeholder="Enter email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <FontAwesomeIcon icon={faLock} />
+                </span>
+              </div>
+              <input
+                type="password"
+                class="form-control"
+                id="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              class="btn btn-success"
+              style={{ width: "100%" }}
+            >
+              Vai!
+            </button>
+          </form>
+        </div>
       </div>
-
-      <form className="wrapper-form" onSubmit={handleSubmit}>
-      <div class="input-group">
-
-      <div class="input-group-prepend">
-            <span class="input-group-text">
-              <FontAwesomeIcon icon={faUser} />
-            </span>
-          </div>
-          <input
-            type="email"
-            class="form-control"
-            id="username"
-            placeholder="Enter email"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </div>
-
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <span class="input-group-text">
-              <FontAwesomeIcon icon={faLock} />
-            </span>
-          </div>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="Enter your password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-
-        <Link to="/homeUser">
-         
-        <button type="submit" class="btn btn-success" style={{width: '100%'}}>
-          {" "}
-          Sign Up{" "}
-        </button>
-        </Link>
-      </form>
-    </div>
     </div>
   );
 }
--- src/component/splash.js
+++ src/component/splash.js
@@ -6,20 +6,38 @@ import { Link } from "react-router-dom";
 export function Splash() {
   return (
     <div className="wrapper-splash">
-    <div className="splash-container">
-      <div className="container-logo"></div>
-      <div className="container-button">
-        <Link to="/signUpTaxi">
-          <button type="button" class="btn btn-success">
-          <i class="fa-sharp fa-solid fa-taxi"></i>
-          </button>
-        </Link>
+      <div className="wrapper-splash-animation">
+      <div className="wrapper-splash-animation-2">
 
-        <Link to="/signUpUser">
-          <button type="button" class="btn btn-success">
-          <i class="fa-solid fa-user"></i>          
-          </button>
-        </Link>
+        <div className="splash-container">
+          <div className="container-logo"></div>
+          <div class="row">
+            <div class="col-6">
+              <Link to="/signUpUser">
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  style={{ width: "100%" }}
+                >
+                  <i className="fas fa-user"></i>
+                  <br /> Area Utenti
+                </button>
+              </Link>
+            </div>
+            <div class="col-6">
+              <Link to="/signUpTaxi">
+                <button
+                  type="button"
+                  className="btn btn-success"
+                  style={{ width: "100%" }}
+                >
+                  <i className="fas fa-taxi"></i>
+                  <br /> Area Tassisti
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     </div>
