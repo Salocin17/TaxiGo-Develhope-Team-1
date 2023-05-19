8482d84ba3415b050a7ca5b0714c9157a9a71251
camelia.mkhalfi <camelia.mkhalfi@gmail.com>
2023-05-19 19:30:19 +0200

    Modify page storico

--- src/component/storico.js
+++ src/component/storico.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect }  from 'react';
-import { Navbar } from "./navBar";
-import Sidebar from "./sidebar";
+import React, { useState, useEffect } from "react";
 import "../css/homeUser.css";
 import "../css/storico.css";
-import { PaymentHistory } from './paymentHistory';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import { PaymentHistory } from "./paymentHistory";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Link } from "react-router-dom";
 
 export function Storico() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
 
-  
   useEffect(() => {
     const token = localStorage.getItem("token");
     fetch(`http://localhost:3300/api/user`, {
@@ -20,44 +18,82 @@ export function Storico() {
       headers: {
         authorization: `Bearer ${token}`,
       },
-    }).then((response) => response.json())
-    .then((data) => {
-      setUsername(data.last_name);       
-      setName(data.first_name); 
-
     })
+      .then((response) => response.json())
+      .then((data) => {
+        setUsername(data.last_name);
+        setName(data.first_name);
+      });
   }, []);
 
-function handleDateChange(date) {
-  setSelectedDate(date);
-}
+  function handleDateChange(date) {
+    setSelectedDate(date);
+  }
 
   const [payments, setPayments] = useState([
-    { date: '2023-05-07', taxiCompany: 'Yellow Cab', origin: 'JFK Airport', destination: 'Manhattan', price: '50.00', departureTime: '10:00 a.m', arrivalTime: '13:09 p.m', state: 'Confermato' },
-    { date: '2023-04-22', taxiCompany: 'Uber', origin: 'LaGuardia Airport', destination: 'Brooklyn', price: '35.00', departureTime: '04:00 a.m', arrivalTime: '10:34 p.m', state: 'Cancellato'  },
-    { date: '2023-03-10', taxiCompany: 'Lyft', origin: 'Newark Airport', destination: 'Jersey City', price: '45.00' , departureTime: '15:00 p.m', arrivalTime: '22:15 p.m', state: 'Completato' },
+    {
+      date: "2023-05-07",
+      taxiCompany: "Yellow Cab",
+      origin: "JFK Airport",
+      destination: "Manhattan",
+      price: "50.00",
+      departureTime: "10:00 a.m",
+      arrivalTime: "13:09 p.m",
+      state: "Confermato",
+    },
+    {
+      date: "2023-04-22",
+      taxiCompany: "Uber",
+      origin: "LaGuardia Airport",
+      destination: "Brooklyn",
+      price: "35.00",
+      departureTime: "04:00 a.m",
+      arrivalTime: "10:34 p.m",
+      state: "Cancellato",
+    },
+    {
+      date: "2023-03-10",
+      taxiCompany: "Lyft",
+      origin: "Newark Airport",
+      destination: "Jersey City",
+      price: "45.00",
+      departureTime: "15:00 p.m",
+      arrivalTime: "22:15 p.m",
+      state: "Completato",
+    },
   ]);
 
   return (
-    <div className="container">
-    <Sidebar />
+    <div className="wrapper-storico">
+      <div className="container-nav-storico">
+        <div className="nav-back">
+          <Link
+            to="/homeUser"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            <i class="fa-solid fa-chevron-left fa-lg"></i>
+          </Link>
+        </div>
+        <div className="nav-title-storico">
+          <div className="nav-text">
+            <h3>Storico</h3>
+          </div>
+          <div className="nav-icon">
+            <DatePicker
+              selected={selectedDate}
+              onChange={handleDateChange}
+              dateFormat="yyyy-MM-dd"
+              isClearable
+              placeholderText="Select a date"
+              className="dataPicker"
+            />
+          </div>
+        </div>
+      </div>
 
-    <div className="container-right">
-      <Navbar name={name} username={username}  />
-    <div className="container-box">
-    <div className="container-box-nav">
-  <h2>Storico</h2>
-  <DatePicker
-    selected={selectedDate}
-    onChange={handleDateChange}
-    dateFormat="yyyy-MM-dd"
-    isClearable
-    placeholderText="Select a date"
-   />
-</div>
-<PaymentHistory payments={payments} selectedDate={selectedDate} />
+      <div className="container-storico">
+        <PaymentHistory payments={payments} selectedDate={selectedDate} />
+      </div>
     </div>
-    </div>
-</div>
   );
 }
