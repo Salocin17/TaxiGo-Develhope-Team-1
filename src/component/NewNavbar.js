d2517aab952a96941d210b180880849fe690a1fb
Nicolas Zarone <nikolaszarone@gmail.com>
2023-05-21 16:29:42 +0200

    fixed navbar logo

--- src/component/NewNavbar.js
+++ src/component/NewNavbar.js
@@ -4,8 +4,8 @@ import '../css/NewNavbar.css';
 
 const NewNavbar = () => {
     return (
-        <div className="navbar navbar-expand-lg navbar-light fixed-top gradient-navbar">
-                <div className="logo-image position-absolute" />
+        <div className="navbar fixed-top gradient-navbar">
+                <div className="logo-image position-absolute"/>
         </div>
     );
 };
